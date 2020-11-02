df7f635e30f1d4a3eabbeb2f11b4613b63f7c0dc
ryanb099 <ryekicks@gmail.com>
2020-11-02 13:22:19 -0500

    1 file modified

--- js/add-element.js
+++ js/add-element.js
@@ -3,33 +3,33 @@
  */
 
 // get all the hot class elements- change them to cool class
+$("li.hot").removeClass("hot");
+$("li").addClass("cool");
 
-
+/*
+var nlist= document.querySelectorAll("li.hot");
+for (var i=0; i<nlist.length; i++)
+{
+  nlist[i].setAttribute("class", "cool");
+}
+*/
 // traverse the elements
 
-
 // add a new element
-
 
 // add by clicking the plus sign
 document.getElementById("add").addEventListener("click", addElement);
 
 function addElement() {
   // add a new element
-  
 }
-
 
 //  click the li element will change the changeStyle
-function changeStyle() {
-
-}
+function changeStyle() {}
 
 // delete by clicking the trash can
 document.getElementById("remove").addEventListener("click", removeElement);
 
 function removeElement() {
   // remove the marked element
- 
- 
 }
