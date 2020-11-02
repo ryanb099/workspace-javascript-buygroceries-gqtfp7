6b1cbde2e684680bef122f2cc29669cd65700a46
ryanb099 <ryekicks@gmail.com>
2020-11-02 12:52:47 -0500

    Initial commit

---
+++ js/add-element.js
@@ -0,0 +1,35 @@
+/*
+ * Practice on Elements
+ */
+
+// get all the hot class elements- change them to cool class
+
+
+// traverse the elements
+
+
+// add a new element
+
+
+// add by clicking the plus sign
+document.getElementById("add").addEventListener("click", addElement);
+
+function addElement() {
+  // add a new element
+  
+}
+
+
+//  click the li element will change the changeStyle
+function changeStyle() {
+
+}
+
+// delete by clicking the trash can
+document.getElementById("remove").addEventListener("click", removeElement);
+
+function removeElement() {
+  // remove the marked element
+ 
+ 
+}
