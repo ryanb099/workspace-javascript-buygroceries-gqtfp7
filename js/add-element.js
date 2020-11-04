73ff75be1414c6981a1dc9bb3a0496e86bfd5a71
ryanb099 <ryekicks@gmail.com>
2020-11-04 13:09:15 -0500

    1 file modified

--- js/add-element.js
+++ js/add-element.js
@@ -3,9 +3,19 @@
  */
 
 // get all the hot class elements- change them to cool class
-$("li.hot").removeClass("hot");
-$("li").addClass("cool");
-//Novermber 2 2020
+$(".hot").each(function() {
+  $(this).removeClass("hot");
+  $(this).addClass("cool");
+});
+
+// traverse the elements
+$("#one")
+  .next()
+  .next()
+  .text("milk");
+
+$("add").click(addElement);
+//Novermber 4 2020
 /*
 var nlist= document.querySelectorAll("li.hot");
 for (var i=0; i<nlist.length; i++)
@@ -21,15 +31,42 @@ for (var i=0; i<nlist.length; i++)
 document.getElementById("add").addEventListener("click", addElement);
 
 function addElement() {
+  $("#todo").append("<li> <input type=text></li>");
   // add a new element
-}
 
+  $("input").blur(function() {
+    $(this)
+      .parent()
+      .addClass("cool");
+
+    var userInput = $(this).val();
+    $(this)
+      .parent()
+      .text(userInput);
+    $("li").click(changeStyle);
+  });
+}
+$("li").click(changeStyle);
 //  click the li element will change the changeStyle
-function changeStyle() {}
+function changeStyle() {
+  if ($(this).hasClass("complete")) {
+    $(this).removeClass("complete");
+    $(this).addClass("cool");
+  } else {
+    $(this).addClass("complete");
+  }
+  if ($(this).hasClass("hot")) {
+    $(this).removeClass("hot");
+    $(this).addClass("complete");
+  } else {
+    $(this).addClass("complete");
+  }
+}
 
 // delete by clicking the trash can
 document.getElementById("remove").addEventListener("click", removeElement);
 
 function removeElement() {
+  $("li.complete").remove();
   // remove the marked element
 }
