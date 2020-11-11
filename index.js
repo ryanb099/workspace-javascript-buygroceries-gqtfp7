471fc9b8d418a07d998ba11029872aaff03b9435
ryanb099 <ryekicks@gmail.com>
2020-11-11 13:04:06 -0500

    1 file modified

--- index.js
+++ index.js
@@ -17,12 +17,29 @@ import * as firebaseui from "firebaseui";
 
 // Add Firebase project configuration object here
 var firebaseConfig = {
-
+  apiKey: "AIzaSyC8YxgOtzn_OanAlh7Q-LpjMN1PFA2_TCM",
+  authDomain: "grocerylist-238b9.firebaseapp.com",
+  databaseURL: "https://grocerylist-238b9.firebaseio.com",
+  projectId: "grocerylist-238b9",
+  storageBucket: "grocerylist-238b9.appspot.com",
+  messagingSenderId: "206619415585",
+  appId: "1:206619415585:web:e839424c83a5c5b780ff60",
+  measurementId: "G-NBECYCE2YS"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-
-
 // Save the list to database
+$("#save").click(function() {
+  $('li').each(function(){
+    var value= $(this).text();
+    console.log(value);
 
+    firebase
+    .firestore()
+    .collection("mylist")
+    .add({
+      item: value
+  });
+  });
+});
